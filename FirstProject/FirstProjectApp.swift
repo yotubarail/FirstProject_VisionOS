f2e0fdc0d27aa694caed33f88907538386925560
yotubarail <shota_t@i.softbank.jp>
2023-07-06 20:36:54 +0900

    Initial Commit

---
+++ FirstProject/FirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstProjectApp.swift
+//  FirstProject
+//
+//  Created by 滝浪翔太 on 2023/07/06.
+//
+
+import SwiftUI
+
+@main
+struct FirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
